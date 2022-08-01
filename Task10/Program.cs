59bd003c0525486adae82e45ed36ab0637b7ac81
«evgeny» <solov-ghenya195@mail.ru>
2022-08-01 22:10:02 +0300

    add Task13

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int IsNumb(int num)
+{
+    int a = num % 100;
+    int b = a /10;
+
+    return b;
+}
+
+int number = new Random().Next(100,999);
+int result = IsNumb(number);
+
+Console.WriteLine(number);
+Console.WriteLine(result);
