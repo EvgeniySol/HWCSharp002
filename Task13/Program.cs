b8835465f228020b1f8b3b3842e3602ba9049841
«evgeny» <solov-ghenya195@mail.ru>
2022-08-02 07:54:08 +0300

    add Task15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,12 +1,12 @@
-﻿// int ThirdDigit(int num)
-// {
-//     int b = x / 100;
-//     int a = x % 100;
-//     int c = a % 10;
-//     int sum = b * 10 + c;
+﻿// string ThirdDigit(int num)
+//  {
+//      int b = x / 100;
+//      int a = x % 100;
+//      int c = a % 10;
+//      int sum = b * 10 + c;
 
 //     return sum;
-// }
+//  }
 
 // int number = new Random().Next(100,999);
 // int gtr = IsNumb(number);
@@ -15,10 +15,8 @@
 // Console.WriteLine(gtr);
 
 
-int x = 1645;
-int third = -1;
-if(x > 99)
-{
-    third = ((x / 100) % 10);
-}
-Console.WriteLine(third);
+int x = 6;
+//string third = GetSecondDigit(x);
+string str = x.ToString();
+Console.WriteLine(str[2]);
+//Console.WriteLine(third);
---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿string Week(int num)
+{
+   if(num == 1 || num == 2 || num ==3 || num == 4 || num == 5) return ("No");
+   else if(num == 6 || num == 7) return ("Yes");
+   else return ("There is no such day of the week!");
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = Week(number);
+
+Console.WriteLine(result);
